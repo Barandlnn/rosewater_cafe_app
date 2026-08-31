4c8a719f20fa72853fa3fcb5fae265315d2761e1
Barandlnn <brndilan@gmail.com>
2026-08-31 21:20:45 +0300

    Add Firestore door access requests

---
+++ lib/services/door_access_service.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DoorAccessService {
+  static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  static Future<String> createAccessRequest({
+    required String uid,
+    required String memberId,
+    required String membershipPlan,
+    required int guestCount,
+  }) async {
+    final expiresAt = DateTime.now().add(const Duration(minutes: 5));
+
+    final documentReference = await _firestore
+        .collection('doorAccessRequests')
+        .add({
+          'userId': uid,
+          'memberId': memberId,
+          'membershipPlan': membershipPlan,
+          'guestCount': guestCount,
+          'status': 'pending',
+          'createdAt': FieldValue.serverTimestamp(),
+          'expiresAt': Timestamp.fromDate(expiresAt),
+        });
+
+    return documentReference.id;
+  }
+}
